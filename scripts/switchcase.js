eee5734f19715009a5b5923beb8aca04d78a981b
Geethika <geethika.duddupudi@zensar.com>
2020-07-22 18:15:15 +0530

    Creation of switch case

---
+++ scripts/switchcase.js
@@ -0,0 +1,13 @@
+var x = "0";
+
+switch (x) {
+  case 0:
+    text = "Off";
+    break;
+  case 1:
+    text = "On";
+    break;
+  default:
+    text = "No value found";
+}
+console.log(`The value of x is ${x}`)
